9f126f76c75fb9a8766fce7717b49afac6f57b16
Fabiano Andrade <affatecnologia@outlook.com.br>
2025-06-07 20:31:08 -0300

    feat: add botões de editar e excluir na tela de descrição

--- Pages/TaskDetailsPage.xaml.cs
+++ Pages/TaskDetailsPage.xaml.cs
@@ -1,24 +1,53 @@
-﻿
+﻿using Tarefas.Constants;
+using Tarefas.Enums;
 using Tarefas.Models;
+using Tarefas.Servicos;
 
 namespace Tarefas.Pages;
 
 public partial class TaskDetailsPage : ContentPage
 {
 	public Tarefa Tarefa { get; set; }
+	DatabaseServico<Tarefa> _databaseServico;
 	public TaskDetailsPage(Tarefa tarefa)
 	{
 		InitializeComponent();
 
 		Tarefa = tarefa;
 		BindingContext = this;
+		_databaseServico = new DatabaseServico<Tarefa>(Db.DB_PATH);
 	}
 
-	private async void GoBackClicked(object sender, EventArgs e)
+	protected override void OnAppearing()
+	{
+		base.OnAppearing();
+		LabelTitulo.Text = Tarefa.Titulo;
+		LabelNomeUsuario.Text = Tarefa.NomeUsuario;
+		LabelDataCriacao.Text = Tarefa.DataCriacao.ToString();
+		LabelDataAtualizacao.Text = Tarefa.DataAtualizacao.ToString();
+		LabelStatus.Text = Tarefa.Status.ToString();
+		LabelDescricao.Text = Tarefa.Descricao;
+    }
+
+	private async void editClicked(object sender, EventArgs e)
+	{
+		await Navigation.PushAsync(new NewTaskPage(Tarefa));
+	}
+
+	private async void deleteClicked(object sender, EventArgs e)
+	{
+		bool confirm = await DisplayAlert("Confirmação", "Deseja excluir esta tarefa?", "Sim", "Não");
+		if (confirm)
+		{
+			await _databaseServico.DeleteAsync(Tarefa);
+			await Navigation.PopAsync();
+		}
+	}
+
+		private async void GoBackClicked(object sender, EventArgs e)
 	{
 		await Navigation.PopAsync();
 	}
-
 	
 }
 
